37d396690bb081119782a7960961495af949bc90
ArthurMazot <arthurmazot@hotmail.com>
2026-08-18 18:38:23 -0300

    Add audio processing and FFT analysis in Atv3.m

---
+++ Atv3.m
@@ -0,0 +1,41 @@
+clc
+clear all
+close all
+
+%% B)
+[x, fs, bits]  = wavread('Bagatelle-no.-25-__Für-Elise___-WoO-59.wav'); %Le o arquivo .wav
+
+bCall = x(:,1);
+tb = (0:1/fs:(length(bCall)-1)/fs);
+
+
+figure(1)
+plot(tb, bCall)
+xlim([0 tb(end)])
+xlabel('Tempo (segundos)')
+ylabel('Amplitude')
+title('{\bf Audio}')
+
+%sound(bCall, fs);
+
+%% C)
+
+m = length(bCall); %
+n = pow2(nextpow2(m)); %Corrige para a próxima potencia de 2
+
+y = fft(bCall, n);
+f = (0:n-1)*(fs/n);
+p = y.*conj(y)/n;
+p = p./max(p);
+
+figure(2)
+subplot(2,1,1)
+plot(f(1:floor(n/2)), p(1:floor(n/2)))
+xlabel('Frequência (Hz)')
+
+subplot(2,1,2)
+plot(f(1:floor(n/2)), phase(y(1:floor(n/2))))
+
+%% D)
+
+
