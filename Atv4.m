b5c79d7bf7c0353bd57488fa00fed711a2983a87
ArthurMazot <arthurmazot@hotmail.com>
2026-09-03 18:10:37 -0300

    Add files via upload

--- Atv4.m
+++ Atv4.m
@@ -4,7 +4,7 @@ close all
 
 [x, fa] = audioread('Bagatelle-no.-25-__Für-Elise___-WoO-59.wav');
 
-%% A)
+%% Escolha das frequecias dos filtros
 
 n = pow2(nextpow2(length(x(:,1)))); %Corrige para a próxima potencia de 2
 
@@ -34,14 +34,20 @@ fpp1 = 2*600/fa;
 fpp2 = 2*800/fa;
 fsp2 = 2*1200/fa;
 
-%Rejeita-Banda (fpr1*fprs2 = fsr1*fsr2)
+%Rejeita-Banda (fpr1*fpr2 = fsr1*fsr2)
 fpr1 = 2*1300/fa;
 fsr1 = 2*1500/fa;
 
 fsr2 = 2*1700/fa;
 fpr2 = 2*1961/fa;
 
-%% B)
+%% Projeto dos filtros FIR
+
+%Escolha da janela: hanning
+%8pi/M
+%Todos os filtro projetados tem como menor transisção de 0.0181*pi
+%8pi/0.0181*pi = 441
+%N escolhido: 450
 
 %Passa-Baixas
 hb = fir2(450,[0 fpb fsb 1], [1 1 0 0], hanning(451));
@@ -64,7 +70,7 @@ plot(wp/pi,20*log10(abs(hpf)), 'k')
 plot(wr/pi,20*log10(abs(hrf)), 'g')
 legend('Passa-Baixas', 'Passa-Altas', 'Passa-Banda', 'Rejeita-Banda')
 
-%% C)
+%% Plot das respostas em frequencias dos filtros
 
 y1 = filter(hb, 1, x(:,1));
 y2 = filter(ha, 1, x(:,1));
@@ -106,11 +112,11 @@ p = p./max(p);
 
 plot(f(1:floor(n/2)), p(1:floor(n/2)), 'g')
 
-%% D)
+%% Sistema da fig. 1
 G = [1 1 1 1];
 yt = G(1)*y1 + G(2)*y2 + G(3)*y3 + G(4)*y4;
 
-sound(yt)
+%sound(yt)
 
 %% Gráfico de Atraso
 
